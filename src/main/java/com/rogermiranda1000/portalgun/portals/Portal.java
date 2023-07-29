517a51ad49544576d913f16ae107182186c33113
rogermiranda1000 <rogermirandaperez@hotmail.com>
2023-07-29 20:10:47 +0200

    Merged & changed version

--- src/main/java/com/rogermiranda1000/portalgun/portals/Portal.java
+++ src/main/java/com/rogermiranda1000/portalgun/portals/Portal.java
@@ -2,6 +2,7 @@ package com.rogermiranda1000.portalgun.portals;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 import java.util.function.Function;
 
